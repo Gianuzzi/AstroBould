43f21d8a1c39b7afd59b080acb51906c12ed877c
Emmanuel <egianuzzi@unc.edu.ar>
2025-03-31 19:15:08 -0300

    feat: Mejora el cálculo de Omega y añade validaciones para Omega0 y Omega_end

--- tools/get_omega_damp.py
+++ tools/get_omega_damp.py
@@ -6,26 +6,27 @@ import matplotlib.pyplot as plt
 # Omega = Omega0 + dOmegadt * t
 # 0 = Omega0 + dOmegadt * tau
 # tau = - Omega0 / dOmegadt
+# factor = Omega_end / Omega0
 
 # Set actual parameters
-P_actual = 7.004 / 24.  # [days]
+P_actual = 0.25  # [days]
 Omega_actual = 2 * np.pi / P_actual  # [Rad/day]
 
 # Example parameters
 Prot0_example = P_actual
 Omega0_example = 2 * np.pi / Prot0_example  # [Rad/day]
-dOmegadt_example = - Omega0_example * 0.3 / 1e7  # [Rad/day^2]
+dOmegadt_example = -Omega0_example * 0.3 / 1e7  # [Rad/day^2]
 Omega_end_example = Omega0_example + dOmegadt_example * 1e7  # [Rad/day]
 Prot_end_example = 2 * np.pi / Omega_end_example  # [days]
 
 # Calculate the damping time
-tau_example = - Omega0_example / dOmegadt_example
+tau_example = -Omega0_example / dOmegadt_example
 
 # Set time
-time_end = 365.25 * 5e4  # [yrs]
+time_end = 365.25 * 1e5  # [days]
 
 # My parameters
-as_example = True
+as_example = False
 
 if as_example:
     Omega0 = Omega0_example
@@ -37,26 +38,34 @@ else:
     # Set my parameters
     Omega0 = 0  # [Rad/day]
     Omega_end = Omega_actual  # [Rad/day]
+    factor = 0.85  # Omega_end = Omega0 * factor
 
     # Calculate my initial Omega or end Omega
-    if Omega0 <= 0:
-        Omega0 = Omega_end - dOmegadt_example * time_end
-    else:
-        Omega_end = Omega0 + dOmegadt_example * time_end
+    if Omega0 <= 0 and Omega_end <= 0:
+        raise ValueError("Omega0 or Omega_end must be positive")
+    elif Omega0 <= 0 or Omega_end <= 0:
+        if factor == 0:
+            raise ValueError("Factor must be positive")
+        if factor == 1:
+            raise ValueError("Factor must be different from 1")
+        if Omega0 <= 0:
+            Omega0 = Omega_end / factor
+        else:
+            Omega_end = Omega0 * factor
 
     # Calculate my dOmegadt
     dOmegadt = (Omega_end - Omega0) / time_end
 
 
 # Calculate my damping time
-tau = - Omega0 / dOmegadt
+tau = -Omega0 / dOmegadt
 
 # Calculate change rate
-change_rate = Omega0/Omega_actual
+change_ratio = Omega0 / Omega_actual
 
 # Print results
 print(f"dOmegadt = {dOmegadt:.4e} rad/day^2")
-print(f"Change rate = {change_rate:.7f} == {change_rate*100:.7f}%")
+print(f"Change rate = {change_ratio:.7f} == {change_ratio*100:.7f}%")
 print(f"Time_end = {time_end:.5e} days")
 print("Actual parameters")
 print(f" Omega = {Omega_actual:.7f}")
@@ -75,10 +84,14 @@ print(f" Prot_end = {Prot_end_example:.7f} days")
 print(f" tau_omega_example = {tau_example:.4e} days")
 
 # Plot
-time = np.sort(np.concatenate((
-    np.logspace(-2, np.log10(time_end), 250),
-    np.linspace(0, time_end, 251))
-))
+time = np.sort(
+    np.concatenate(
+        (
+            np.logspace(-2, np.log10(time_end), 250),
+            np.linspace(0, time_end, 251),
+        )
+    )
+)
 
 Omega = Omega0 + dOmegadt * time
 Omega0_example = Omega0_example + dOmegadt_example * time
@@ -89,23 +102,44 @@ tau_base = tau / (10**tau_exponente)
 
 plt.figure(dpi=120, figsize=(8, 6))
 
-plt.gca().axhline(Omega0, color='r', ls='--',
-                  label=f'Initial: {Omega0:.2f} rad/day')
-plt.gca().axhline(Omega_actual, color='k', ls='--',
-                  label=f'Actual: {Omega_actual:.2f} rad/day')
+ax = plt.gca()
 
-plt.plot(time/365.25, Omega0_example, label='Example')
-plt.plot(time/365.25, Omega, label='Mine')
+ax.axhline(
+    Omega0,
+    color="r",
+    ls="--",
+    label=f"Initial: {Omega0:.2f} rad/day " + f"({2*np.pi/Omega0:.5f} days)",
+)
+ax.axhline(
+    Omega_actual,
+    color="k",
+    ls="--",
+    label=f"Actual: {Omega_actual:.2f} rad/day " + f"({P_actual:.5f} days)",
+)
 
-# plt.xscale('log')
+if as_example:
+    plt.plot(time / 365.25, Omega0_example, label="Example")
+plt.plot(time / 365.25, Omega, label="Mine")
 
-plt.title('$d\\Omega/dt = %.2f\\times10^{%d}$ rad/day$^2$' % (
-    dOmegadt_base, dOmegadt_exponente))
+ax2 = ax.secondary_yaxis(
+    "right",
+    functions=(lambda x: 2 * np.pi / x * 24, lambda x: 2 * np.pi / x / 24),
+)
+ax2.set_ylabel("$P$ [hours]")
 
-plt.xlabel('Time [years]')
-plt.ylabel('Omega [rad/day]')
+plt.title(
+    "$d\\Omega/dt = %.4f\\times10^{%d}$ rad/day$^2$"
+    % (dOmegadt_base, dOmegadt_exponente)
+)
 
-plt.legend(title="$\\tau_\\Omega = %.2f\\times10^{%d}$ days" % (
-    tau_base, tau_exponente))
+plt.xlabel("Time [years]")
+plt.ylabel("Omega [rad/day]")
+
+plt.legend(
+    title="$\\tau_\\Omega = %.4f\\times10^{%d}$ days"
+    % (tau_base, tau_exponente)
+)
+
+plt.grid()
 
 plt.show()
